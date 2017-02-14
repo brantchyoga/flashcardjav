eae0f1523ddd37c6cecb206140788ae0b4a19e2c
brant porter and xing li <brant@>
2017-02-14 14:29:21 -0800

    Flash different definitions based on clicking on html elements

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+$(function() {
+  $("#clickable1").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Javascript is a high-level, dynamic, untyped, and interpreted programming language. It has been standardized in ECMAScript language specification.");
+  });
+  $("#clickable2").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Operators are used to assign values, compare values, perform arithmetic operations, and more.");
+  });
+  $("#clickable3").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Variables are containers for storing data values.");
+  });
+  $("#clickable4").click(function() {
+    $("#definition").empty();
+    $("#definition").append("How you name your varianles to make reader easy to read.");
+  });
+  $("#clickable5").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Functions tell or modify objects to creat an outcome.");
+  });
+  $("#clickable6").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Methods are specific known functions that are used on objects.");
+  });
+  $("#clickable7").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Arguments are the values you run through your function to get your outcome.");
+  });
+  $("#clickable8").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Parameters are what you run through your function but aren't specific until you name them.");
+  });
+  $("#clickable9").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Returns are statements that stop the execution of a function and return a value from the function.");
+  });
+  $("#clickable10").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Chaining methods are ways to compress code by putting multiple methods within a single statement.");
+  });
+  $("#clickable11").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Strings are data stores that store series of characters.");
+  });
+  $("#clickable12").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Booleans are a object with 2 values that represent false or true.");
+  });
+  $("#clickable13").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Undefined is an object that hasn't been assigned a value.");
+  });
+  $("#clickable14").click(function() {
+    $("#definition").empty();
+    $("#definition").append("NaN stands for not a number expressing an undefined value.");
+  });
+  $("#clickable15").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Escape is a character used in strigns to make special characters read as string characters. Ex:\"we are the viking.\" Escape character is a backlash.");
+  });
+  $("#clickable16").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Alert is a way to interact with a user by popping up a box that allows the user a way to express a boolean.");
+  });
+  $("#clickable17").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Comments are ways to make notes in your code to make it easier to read down the road or to another developer.");
+  });
+  $("#clickable18").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Jquery is a concise and fast javascript library that can be used to simplify event handling and html document traversing.");
+  });
+  $("#clickable19").click(function() {
+    $("#definition").empty();
+    $("#definition").append("Attributes provide additional information about HTML elements.");
+  });
+});
